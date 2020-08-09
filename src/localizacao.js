00b33246a3e2aecc90ffeadf10c0404a37bdc76f
brunorodriguesdecarvalho <rodrigues_b@hotmail.com>
2020-08-09 20:37:42 -0300

    nova atualização na leitura da API do GMaps

--- src/localizacao.js
+++ src/localizacao.js
@@ -1,37 +1,47 @@
 var map, infoWindow;
-var posicaoInicial = {lat: -23.5677861, lng: -46.7006432};
+var posicaoInicial = {lat: -23.553286, lng: -46.702505};
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: posicaoInicial,
-        zoom: 15
+        zoom: 13
     });
-    new google.maps.Marker({
+
+    infoWindow = new google.maps.InfoWindow;
+
+    //Try HTML5 geolocation.
+    obterLocalAtual();
+
+    var marker = new google.maps.Marker({
         position: posicaoInicial,
-        map,
-        title: "Hello World!"
+        map: map,
+        title: 'Hello World!'
     });
-
-infoWindow = new google.maps.InfoWindow;
-
-//Try HTML5 geolocation.
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-    };
-
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    infoWindow.open(map);
-    map.setCenter(pos);
-    }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-        });
-} else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
 }
+
+function obterLocalAtual() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            console.log("Posição recebida do navegador: Lat->"+ pos.lat+". Lon->"+ pos.lng+".")
+            infoWindow.setContent('Localiazação via Browser Encontrada');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+            console.log("Disparou FUNCTION QUE NÂO DEVIA!!")
+        });
+    }
+    else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+        alert("Não foi possível encontrar a localização atual.")
+    }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -40,4 +50,9 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                             'Error: The Geolocation service failed.' :
                             'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
+}
+
+function teste(){
+    alert("testando e provando")
+    console.log("testando e provando")
 }
